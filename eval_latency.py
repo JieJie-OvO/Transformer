b36713bd78b894950aa91955c879ed66b431b2fb
GuoYujie <1802818706@qq.com>
2024-07-23 11:38:02 +0800

    完成对Latency的衡量

--- eval_latency.py
+++ eval_latency.py
@@ -26,6 +26,7 @@ class Eval_latency():
         self.model.load_model(chkpt)
 
     def recognize(self):
+        idx2unit = self.train_loader.dataset.idx2unit()
         reses = textgrid2outlist(self.pinyin_dic)
         self.model.eval()
         accu_time = 0
@@ -35,36 +36,60 @@ class Eval_latency():
 
         for step, (utt_id, inputs, targets) in enumerate(self.test_loader.loader):
             latency = []
-            print( reses[utt_id])
-            exit()
-            if step == 0:
-                # 最开始跳过，系统还没有稳定
-                continue
-            
             if self.ngpu>0:
                 inputs = map_to_cuda(inputs)
             total_frames = inputs['inputs'].size(1)
 
-            res = reses[utt_id]
+            res = reses[utt_id[0]]
         
             st = time.time()
             preds = self.model.recognize_with_latency(inputs)
 
+            if step == 0:
+                # 最开始跳过，系统还没有稳定
+                continue
+
             pred = preds[0]
+
+            assert len(res) == len(pred) + 1
+
             for i in range(len(pred)):
                 # [[now, t], pred]
                 idx = compute_frames(total_frames, pred[i][0][1])
                 et = pred[i][0][0]
                 delta = idx * 0.01 + et - st
                 min = delta - res[i][1]
-                max = delta - res[i][0]
-                latency.append([min, max])
-                    
+                max = delta - res[i][2]
+                latency.append([idx2unit[pred[i][1]],min, max])
+            
+            latency.append(res[-1])
             latencys.append(latency)
 
         # TODO计算结果
+        FTD = []
+        LTD = []
+        AvgTD = []
+        for latency in latencys:
+            sub_list = []
+            for i in range(len(latency)-1):
+                sub_list.append((latency[i][1]+latency[i][-1])/2 * 1000)
+            FTD.append(sub_list[0])
+            LTD.append(sub_list[-1])
+            avg = 0.
+            for i in sub_list:
+                avg += i
+            AvgTD.append(avg / len(sub_list))
+        
+        idx50 = int(len(FTD) * 0.5)
+        idx90 = int(len(FTD) * 0.9)
+        FTD.sort()
+        LTD.sort()
+        AvgTD.sort()
 
-
+        with open("./log/latency.txt", 'w', encoding='utf-8') as w:
+            w.write('FTD50  :{0}ms        FTD90  :{1}ms\n'.format(FTD[idx50], FTD[idx90]))
+            w.write('LTD50  :{0}ms        LTD90  :{1}ms\n'.format(LTD[idx50], LTD[idx90]))
+            w.write('AvgTD50:{0}ms        AvgTD90:{1}ms\n'.format(AvgTD[idx50], AvgTD[idx90]))
 fbank=80
 input_size=80
 enc_hidden=512
@@ -81,17 +106,17 @@ lmweight = 0.2
 
 train_wav_path = "egs/aishell/data/train/wav.scp"
 train_text_path = "egs/aishell/data/train/text"
-test_wav_path = "eval_latency/test/wav.scp"
-test_text_path = "eval_latency/test/text"
+test_wav_path = "Eval_latency/test/wav.scp"
+test_text_path = "Eval_latency/test/text"
 vab_path = "egs/aishell/data/transducer_vab"
 
-pinyin_dic = "eval_latency/out"
+pinyin_dic = "Eval_latency/out"
 
 batch_size = 10
 train_epochs = 80
 accum_steps = 6
 
-ngpu = 1 if torch.cuda.is_available() else 0
+ngpu = 0 if torch.cuda.is_available() else 0
 print("ngpu: ", ngpu)
 
 model = Transducer(fbank, input_size, enc_hidden, enc_out, enc_layers, 
@@ -102,5 +127,5 @@ model = Transducer(fbank, input_size, enc_hidden, enc_out, enc_layers,
 solver = Eval_latency(model, train_wav_path,train_text_path, test_wav_path, test_text_path,
                 vab_path, fbank, batch_size, ngpu, pinyin_dic=pinyin_dic)
 
-solver.load_model("./pth/model.epoch.4.pth")
+solver.load_model("./result/latency/rnnt.pth")
 solver.recognize()
